4bc6c88c1ba31f2995a2f9d7e63e190c46eb7622
jresinas <jresinas@emergya.com>
2014-09-22 10:23:54 +0200

    Merging with redmine_allocation plugin

--- lib/cpm/hooks.rb
+++ lib/cpm/hooks.rb
@@ -1,5 +1,11 @@
 module CPM
   class Hooks < Redmine::Hook::ViewListener
+    render_on :view_projects_settings_members_table_header,
+              :partial => 'hooks/cpm/view_projects_settings_members_table_header'
+    render_on :view_projects_settings_members_table_row,
+              :partial => 'hooks/cpm/view_projects_settings_members_table_row'
+
+    # Show users dedication in welcome page
     def view_welcome_index_right(context={ })
       content = User.current.get_capacity_summary
 
@@ -9,7 +15,6 @@ module CPM
       	content = "<h3>Dedicación para hoy</h3>".html_safe+content
       end
 
- 
       return content_tag(:div, content, :class  => 'box')
     end
   end
