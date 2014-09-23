363fcb9998aa5a60fe6906679273cab01843c9f9
jresinas <jresinas@emergya.com>
2014-09-23 16:06:21 +0200

    Render Redmine's view hook as a partial view

--- lib/cpm/hooks.rb
+++ lib/cpm/hooks.rb
@@ -1,17 +1,7 @@
 module CPM
   class Hooks < Redmine::Hook::ViewListener
-    def view_welcome_index_right(context={ })
-      content = User.current.get_capacity_summary
-
-      if content.blank?
-      	content = l(:"cpm.label_no_assignments_today")
-      else
-      	content = ("<h3>"+l(:"cpm.label_todays_assignments")+"</h3>"+content).html_safe
-      end
-
- 
-      return content_tag(:div, content, :class  => 'box')
-    end
+    render_on :view_welcome_index_right,
+              :partial => 'hooks/cpm/view_welcome_index_right'
   end
 end
 
